bb9b328cac3632aab6b30430bfcc85ae77712b21
sergiob <sergiob@ciandt.com>
2022-07-01 10:27:02 -0300

    [DayOne] Create layout elements.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Android7daysOfCode
-Programa 7 days of code alutra
+Programa 7 days of code alura
+
+#DAY ONE
+Create initial layout to bind use data.
+It have to do:
+
+User Name
+GitHub User
+Job Description
+
+
--- app/src/main/java/com/alura/githubprofile/MainActivity.kt
+++ app/src/main/java/com/alura/githubprofile/MainActivity.kt
@@ -3,13 +3,23 @@ package com.alura.githubprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alura.githubprofile.ui.theme.GitHubProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GitHubProfileTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.padding(2.dp)) {
+                        ProfileImage(R.drawable.profile_image)
+
+                        //TODO data will be provided by backend.
+                        Text(text = "Sérgio Borges", fontSize = 26.sp)
+                        Text(text = "smborgesMobile", fontSize = 24.sp)
+                        Text(text = "Android Developer at CI&T", fontSize = 22.sp)
+                    }
                 }
             }
         }
@@ -30,14 +46,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun ProfileImage(resourceId: Int) {
+    val painter = painterResource(id = resourceId)
+    Image(
+        painter = painter,
+        contentDescription = "",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GitHubProfileTheme {
-        Greeting("Android")
+        Column(Modifier.padding(2.dp)) {
+            ProfileImage(R.drawable.profile_image)
+
+            //TODO data will be provided by backend.
+            Text(text = "Sérgio Borges", fontSize = 30.sp)
+            Text(text = "smborgesMobile", fontSize = 28.sp)
+            Text(text = "Android Developer at CI&T", fontSize = 26.sp)
+        }
     }
 }
