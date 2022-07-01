b3c946e44433d072a549410089b765689c43e339
sergiob <sergiob@ciandt.com>
2022-07-01 11:13:10 -0300

    Layout Improvement

--- app/src/main/java/com/alura/githubprofile/MainActivity.kt
+++ app/src/main/java/com/alura/githubprofile/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,19 +30,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitHubProfileTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxWidth(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(modifier = Modifier.padding(2.dp)) {
-                        ProfileImage(R.drawable.profile_image)
-
-                        //TODO data will be provided by backend.
-                        Text(text = "Sérgio Borges", fontSize = 26.sp)
-                        Text(text = "smborgesMobile", fontSize = 24.sp)
-                        Text(text = "Android Developer at CI&T", fontSize = 22.sp)
-                    }
+                    ProfileCard(
+                        Modifier
+                            .padding(20.dp)
+                            .fillMaxWidth()
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun ProfileCard(modifier: Modifier) {
+    Card(
+        modifier = modifier
+    ) {
+        Column(Modifier.padding(4.dp)) {
+            ProfileImage(R.drawable.profile_image)
+
+            //TODO data will be provided by backend.
+            Text(text = "Sérgio Borges", fontSize = 26.sp, fontWeight = FontWeight.Bold)
+            Text(text = "smborgesMobile", fontSize = 28.sp)
+            Text(text = "Android Developer at CI&T", fontSize = 26.sp)
         }
     }
 }
@@ -62,13 +77,10 @@ private fun ProfileImage(resourceId: Int) {
 @Composable
 fun DefaultPreview() {
     GitHubProfileTheme {
-        Column(Modifier.padding(2.dp)) {
-            ProfileImage(R.drawable.profile_image)
-
-            //TODO data will be provided by backend.
-            Text(text = "Sérgio Borges", fontSize = 30.sp)
-            Text(text = "smborgesMobile", fontSize = 28.sp)
-            Text(text = "Android Developer at CI&T", fontSize = 26.sp)
-        }
+        ProfileCard(
+            Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        )
     }
 }
